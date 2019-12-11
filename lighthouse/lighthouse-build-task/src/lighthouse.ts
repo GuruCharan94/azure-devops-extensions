e27504dae5790c0e460564d24babc9b92ff2fe02
Gurucharan94 <guru11294@gmail.com>
2019-12-11 21:15:14 +0530

    Add Better Attachment Proceesing Logic

--- lighthouse/lighthouse-build-task/src/lighthouse.ts
+++ lighthouse/lighthouse-build-task/src/lighthouse.ts
@@ -2,9 +2,8 @@ import tasklib = require('azure-pipelines-task-lib/task');
 import toolrunner = require('azure-pipelines-task-lib/toolrunner');
 import * as path from 'path';
 import url=require('url');
-import { chmodSync } from "fs";
+import * as fs from "fs";
 import * as os from "os";
-
 
 async function run() {
     let resultsFolder: string = "lighthouse-reports"
@@ -14,33 +13,39 @@ async function run() {
         let targetURL: string = tasklib.getInput('targetURL', true);
         let configFilePath: string = tasklib.filePathSupplied('configFilePath') ? 
                                         `--config-path ${tasklib.getPathInput('configFilePath', false, true)}` : "" ;
-        let parameters: string = tasklib.getInput('parameters', false) || "";
-        
+        let parameters: string = tasklib.getInput('parameters', false) || "";        
         let isWindows: Boolean = os.platform() === "win32";
-
         let lighthousePath: string = path.join(__dirname, 'node_modules','.bin','lighthouse');
-        if (isWindows) {
-            lighthousePath += ".cmd";
-        } else {
-            chmodSync(lighthousePath, "777");
-        }   
+        
+        
+        if (isWindows) {        
+            lighthousePath += ".cmd";        
+        } else {          
+            fs.chmodSync(lighthousePath, "777");
+        }
 
-        tasklib.rmRF(resultsFolder);
         tasklib.mkdirP(resultsFolder);
         tasklib.cd(resultsFolder);
         
+        let referenceTime = new Date().getTime();
+
         const lighthouse  = tasklib.tool(lighthousePath);
-        lighthouse.arg(targetURL)                
+        lighthouse.arg(targetURL)    
                 .line(`--output json --output html ${parameters} ${configFilePath}`)                
                 .exec()
                 .then(() => {
 
-                    let htmlReports = tasklib.findMatch(tasklib.cwd(),"*.html");                    
-                    let attachmentName = (url.parse(targetURL).hostname! + url.parse(targetURL).path!.replace(/\//g, "_")).slice(0, -1);
-
-                    htmlReports.forEach(report => {
-                        tasklib.addAttachment("gurucharan94.lighthouse-html-artifact", attachmentName, report);
-                    });
+                    let htmlReports = tasklib.findMatch(tasklib.cwd(),"*.html");
+                    htmlReports.forEach(function(file){
+                        let fileCreatedTime = fs.statSync(file).ctime.getTime();
+                        if(fileCreatedTime > referenceTime)
+                        {
+                            let attachmentName = (url.parse(targetURL).hostname! + url.parse(targetURL).path!.replace(/\//g, "_")).slice(0, -1);
+                            htmlReports.forEach(report => {
+                                tasklib.addAttachment("gurucharan94.lighthouse-html-artifact", attachmentName, report);
+                            });
+                        }
+                    })
                 },
                 (error) => {
                     tasklib.setResult(tasklib.TaskResult.Failed, error);           
