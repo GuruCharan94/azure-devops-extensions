ed894ad5de73255bfd4ce79238fba7bc60d4342f
Gurucharan94 <guru11294@gmail.com>
2019-12-24 19:06:57 +0530

    Populate Build Context

---
+++ lighthouse-ci/lighthouse-ci-build-task/src/lighthouse-ci-build-context.ts
@@ -0,0 +1,71 @@
+import tasklib = require('azure-pipelines-task-lib/task');
+import toolrunner = require('azure-pipelines-task-lib/toolrunner');
+import * as path from 'path';
+import url = require('url');
+import * as fs from "fs";
+import * as os from "os";
+import { throws } from 'assert';
+
+export class BuildContext {
+
+    private LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG: string = '';
+    private LHCI_BUILD_CONTEXT__CURRENT_HASH: string = '';
+    private LHCI_BUILD_CONTEXT__COMMIT_TIME: string = '';
+    private LHCI_BUILD_CONTEXT__COMMIT_MESSAGE: string = '';
+    private LHCI_BUILD_CONTEXT__CURRENT_BRANCH: string = '';
+    private LHCI_BUILD_CONTEXT__AUTHOR: string = '';
+    private LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL: string = '';
+
+    constructor(targetArtifactPath: string) {
+
+        if (tasklib.getVariable('BUILD_BUILD')) {
+
+            tasklib.debug('Running Inside a Build Pipeline. Infer Build Context from Git Repo');
+            this.inferBuildContextFromBuild();
+
+        }
+        else {
+            tasklib.debug('Running Inside a Release Pipeline. Infer Build Context from Chosen Artifact');
+            this.inferBuildContextFromRelease(targetArtifactPath);
+        }
+        
+        this.setBuildContextAsTaskVariable();
+    }
+
+    private async inferBuildContextFromBuild() {
+        this.LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG = tasklib.getVariable('BUILD__REPOSITORY_NAME');
+        this.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = `${tasklib.getVariable('SYSTEM_COLLECTIONURI')}${tasklib.getVariable('SYSTEM_TEAMPROJECT')}/_build/results?buildId=${tasklib.getVariable('BUILD_BUILDID')}`;
+        // other variables are inferred from git by LHCI.
+    }
+
+    private async inferBuildContextFromRelease(targetArtifactPath: string) {
+        let artifactAlias: string;
+
+        // Choose Provided
+        if (!targetArtifactPath) {
+            artifactAlias = tasklib.getVariable('RELEASE_PRIMARYARTIFACTSOURCEALIAS').toUpperCase();
+        }
+        else {
+            artifactAlias = path.basename(tasklib.getVariable('targetArtifactPath')).toUpperCase();
+        }
+
+        this.LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG = tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_REPOSITORY_NAME}`);
+        this.LHCI_BUILD_CONTEXT__CURRENT_HASH = tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_SOURCEVERSION`);
+        this.LHCI_BUILD_CONTEXT__COMMIT_TIME = tasklib.getVariable(`RELEASE_DEPLOYMENT_STARTTIME`);
+        this.LHCI_BUILD_CONTEXT__COMMIT_MESSAGE = `${tasklib.getVariable('RELEASE_DEFINITIONNAME')} - ${tasklib.getVariable('RELEASE_RELEASENAME')} - ${tasklib.getVariable('RELEASE_ENVIRONMENTNAME')}`;
+        this.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_SOURCEBRANCHNAME`);
+        this.LHCI_BUILD_CONTEXT__AUTHOR = `${tasklib.getVariable('RELEASE_RELEASENAME')}`;
+        this.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = tasklib.getVariable('RELEASE_RELEASEWEBURL');
+    }
+
+    private async setBuildContextAsTaskVariable()
+    {
+        for (var key of Object.keys(this)) {
+
+            if (key.startsWith('LHCI_BUILD_CONTEXT') && this[key]) {
+                tasklib.debug(key + " -> " + this[key]);
+                tasklib.setTaskVariable(key, this[key], false);
+            }
+        }
+    }
+}
--- lighthouse-ci/lighthouse-ci-build-task/src/lighthouse-ci.ts
+++ lighthouse-ci/lighthouse-ci-build-task/src/lighthouse-ci.ts
@@ -4,50 +4,67 @@ import * as path from 'path';
 import url = require('url');
 import * as fs from "fs";
 import * as os from "os";
+import { throws } from 'assert';
+import { BuildContext } from './lighthouse-ci-build-context';
 
-async function run() {
-    try {
-        
-        // Read Inputs
-        let command: string = tasklib.getInput('command');
-        let configFilePath: string = tasklib.filePathSupplied('configFilePath') ?
+export class lighthouseCI {
+
+    private command: string;
+    private configFilePath: string;
+    private parameters: string;
+    private targetArtifact: string;
+
+    constructor() {
+        this.command = tasklib.getInput('command');
+
+        this.configFilePath = tasklib.filePathSupplied('configFilePath') ?
             `--config ${tasklib.getPathInput('configFilePath', false, true)}` : "";
-        let parameters: string = tasklib.getInput('parameters', false) || "";
-        let targetArtifact: string = tasklib.getInput('')
-        
-        // LHCI CWD Prep
-        let LHCI_DIR = tasklib.getTaskVariable('LHCI_DIR');
-        if(!LHCI_DIR)
-        {
-            LHCI_DIR = tasklib.cwd(); // Directory inside which lighthouse is executed.
-            if (configFilePath) {
-                // If path to Lighthouse CI config is provided, change cwd to the folder containing the file.
-                // All path to related files, Lighthouse & Puppeteer Config is there.
-                LHCI_DIR=path.dirname(tasklib.getPathInput('configFilePath'));
-            }
+
+        this.parameters = tasklib.getInput('parameters', false) || "";
+
+        this.targetArtifact = tasklib.filePathSupplied('targetArtifactPath') ?
+            `${tasklib.getPathInput('targetArtifactPath', false, true)}` : "";
+
+        if (!tasklib.getTaskVariable('LHCI_BUILD_CONTEXT_FLAG')) { // If Variable not set in previous task.
+            new BuildContext(this.targetArtifact);
+            tasklib.setTaskVariable("LHCI_BUILD_CONTEXT_FLAG", 'SET', false); // Set Variable
         }
-        tasklib.cd(LHCI_DIR);
-
-        // Get settings
-        
-
-
-        // Execute Lighthouse
-        tasklib.setTaskVariable("LHCI_DIR",LHCI_DIR, false);
-        let lighthouse = tasklib.tool('lhci');
-        lighthouse
-            .line(`${command} ${configFilePath} ${parameters}`)
-            .exec()
-            .then(() => {
-            },
-                (error) => {
-                    tasklib.setResult(tasklib.TaskResult.Failed, error);
-                }
-            )
     }
-    catch (error) {
-        tasklib.setResult(tasklib.TaskResult.Failed, error.message);
+
+
+    public async run() {
+
+        try {
+            // LHCI CWD Prep
+            let LHCI_DIR = tasklib.getTaskVariable('LHCI_DIR');
+            if (!LHCI_DIR) {
+                LHCI_DIR = tasklib.cwd(); // Directory inside which lighthouse is executed.
+                if (this.configFilePath) {
+                    // If path to Lighthouse CI config is provided, change cwd to the folder containing the file.
+                    // All path to related files, Lighthouse & Puppeteer Config is there.
+                    LHCI_DIR = path.dirname(tasklib.getPathInput('configFilePath'));
+                }
+            }
+            tasklib.cd(LHCI_DIR);
+
+            // Execute Lighthouse
+            tasklib.setTaskVariable("LHCI_DIR", LHCI_DIR, false);
+            let lighthouse = tasklib.tool('lhci');
+            lighthouse
+                .line(`${this.command} ${this.configFilePath} ${this.parameters}`)
+                .exec()
+                .then(() => {
+                },
+                    (error) => {
+                        tasklib.setResult(tasklib.TaskResult.Failed, error);
+                    }
+                )
+        }
+        catch (error) {
+            tasklib.setResult(tasklib.TaskResult.Failed, error.message);
+        }
     }
 }
 
-run();
+var exe = new lighthouseCI();
+exe.run().catch((reason) => tasklib.setResult(tasklib.TaskResult.Failed, reason));
