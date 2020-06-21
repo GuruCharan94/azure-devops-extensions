9608c5a121a3f82407ed8fb92a156f8ac56c8da2
Gurucharan Subramani <guru11294@gmail.com>
2020-06-21 13:35:54 +0530

    Fix Build Context Failures in YAML Multi Stage Pipelines (#16)

--- lighthouse-ci/lighthouse-ci-build-task/src/lighthouse-ci-build-context.ts
+++ lighthouse-ci/lighthouse-ci-build-task/src/lighthouse-ci-build-context.ts
@@ -12,28 +12,31 @@ export class LightHouseCIBuildContext {
     private LHCI_BUILD_CONTEXT__CURRENT_HASH: string = '';
     private LHCI_BUILD_CONTEXT__COMMIT_TIME: string = '';
     private LHCI_BUILD_CONTEXT__COMMIT_MESSAGE: string = '';
-    private LHCI_BUILD_CONTEXT__CURRENT_BRANCH: string = ''; 
+    private LHCI_BUILD_CONTEXT__CURRENT_BRANCH: string = '';
     private LHCI_BUILD_CONTEXT__AUTHOR: string = '';
     private LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL: string = '';
 
     constructor(targetArtifactPath: string) {
 
         if (tasklib.getVariable('RELEASE_RELEASEID')) {
-
-            tasklib.debug('Running Inside a Release Pipeline. Will infer Build Context from Artifact');
+            tasklib.debug('Running Inside the classic Release Pipeline');
             this.inferBuildContextFromRelease(targetArtifactPath);
         }
         else {
-            tasklib.debug('Running Inside a Build Pipeline. Will infer Build Context from Git Repo');
             this.inferBuildContextFromBuild();
         }
     }
 
-    private  inferBuildContextFromBuild() {
+    private inferBuildContextFromBuild() {
         this.LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG = tasklib.getVariable('LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG') || tasklib.getVariable('BUILD_REPOSITORY_NAME');
         this.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = tasklib.getVariable('LHCI_BUILD_CONTEXT__CURRENT_BRANCH') || tasklib.getVariable('BUILD_SOURCEBRANCHNAME');
         this.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = `${tasklib.getVariable('SYSTEM_COLLECTIONURI')}${tasklib.getVariable('SYSTEM_TEAMPROJECT')}/_build/results?buildId=${tasklib.getVariable('BUILD_BUILDID')}`;
-        // other variables are inferred from git by LHCI.
+
+        this.LHCI_BUILD_CONTEXT__COMMIT_MESSAGE = tasklib.getVariable('BUILD_BUILDNUMBER');
+        this.LHCI_BUILD_CONTEXT__COMMIT_TIME = new Date().toISOString();
+        this.LHCI_BUILD_CONTEXT__AUTHOR = tasklib.getVariable('BUILD_BUILDNUMBER');
+        this.LHCI_BUILD_CONTEXT__CURRENT_HASH = tasklib.getVariable('BUILD_SOURCEVERSION');
+        
     }
 
     private inferBuildContextFromRelease(targetArtifactPath: string) {
@@ -52,7 +55,7 @@ export class LightHouseCIBuildContext {
 
         this.LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG = tasklib.getVariable('LHCI_BUILD_CONTEXT__GITHUB_REPO_SLUG') || tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_REPOSITORY_NAME`);
         this.LHCI_BUILD_CONTEXT__CURRENT_BRANCH = tasklib.getVariable('LHCI_BUILD_CONTEXT__CURRENT_BRANCH') || tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_SOURCEBRANCH`);
-        
+
         this.LHCI_BUILD_CONTEXT__AUTHOR = tasklib.getVariable('RELEASE_RELEASENAME');
         this.LHCI_BUILD_CONTEXT__CURRENT_HASH = tasklib.getVariable(`RELEASE_ARTIFACTS_${artifactAlias}_SOURCEVERSION`);
         this.LHCI_BUILD_CONTEXT__COMMIT_TIME = tasklib.getVariable(`RELEASE_DEPLOYMENT_STARTTIME`);
@@ -60,8 +63,7 @@ export class LightHouseCIBuildContext {
         this.LHCI_BUILD_CONTEXT__EXTERNAL_BUILD_URL = tasklib.getVariable('RELEASE_RELEASEWEBURL');
     }
 
-    public setBuildContext()
-    {        
+    public setBuildContext() {
         for (var key of Object.keys(this)) {
 
             if (key.startsWith('LHCI_BUILD_CONTEXT') && this[key]) {
